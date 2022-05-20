8d4a8a4d7eca0d9a1d3dd03d4867f820372bfc47
jmartinrg <82645056+jmartinrg@users.noreply.github.com>
2022-05-20 13:08:43 +0200

    Update Calculadora.java
    Eliminados gets y sets

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -12,22 +12,7 @@ public class Calculadora {
     }
 
 
-    // sets and gets
-
-    public void setNumero1(double numero1) {
-        this.numero1=numero1;
-    }
-    public void setNumero2(double numero2) {
-        this.numero2=numero2;
-    }
-
-    public double getNumero1(){
-        return numero1;
-    }
-    public double getNumero2(){
-        return numero2;
-    }
-
+    
     //métodos
 
     public double suma() {
